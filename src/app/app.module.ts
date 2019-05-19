73a653184e0f9fd7e090dad7801928a57a05e16d
Jason Kirk <JasonK4151@gmail.com>
2019-05-20 00:58:11 +0100

    Added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+// Pre-Defined Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// App Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// Third Party Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
