346555a31b45cc0a301a69911fd37ee39682679d
Jason Kirk <JasonK4151@gmail.com>
2019-12-10 23:12:16 +0000

    Dynamically add next 7 days of week

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -9,15 +9,25 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 export class MatchesComponent implements OnInit {
 
+	daysOfWeek: string[] = [];
+
 	constructor() { }
 
 	ngOnInit() {
+		this.daysOfWeek = this.generateDates();
 	}
 
-	setDate( text: string ): string {
-		const date: number = new Date().getDate();
-		console.log( `${ text } - ${ date }${ this.nth( date ) }` );
-		return `${ text } - ${ date }${ this.nth( date ) }`;
+	generateDates(): string[] {
+		const daysOfWeek = [];
+		for ( let i = 0; i < 7; i++ ) {
+			const date: Date = new Date();
+			const day = date.getDate() + i;
+			const options = { weekday: 'long', month: 'long', day: 'numeric' };
+
+			date.setDate( day );
+			daysOfWeek.push( date.toLocaleDateString( 'en-GB', options ) );
+		}
+		return daysOfWeek;
 	}
 
 	nth( date: number ): string {
